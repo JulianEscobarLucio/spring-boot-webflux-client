cf5a788fb6974e4e226c3a4da71f97ed75118909
julieslu <jescobar@ias.com.co>
2021-11-18 21:22:02 -0500

    first commit

---
+++ src/main/java/com/bolsadeideas/springboot/webflux/cliente/app/model/Producto.java
@@ -0,0 +1,62 @@
+package com.bolsadeideas.springboot.webflux.cliente.app.model;
+
+import java.util.Date;
+
+public class Producto {
+
+	private String id;
+	private String nombre;
+	private Double precio;
+	private String foto;
+	private Categoria categoria;
+	private Date creatAt;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Date getCreatAt() {
+		return creatAt;
+	}
+
+	public void setCreatAt(Date creatAt) {
+		this.creatAt = creatAt;
+	}
+
+}
